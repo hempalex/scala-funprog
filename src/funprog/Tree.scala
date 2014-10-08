c898f4dd75583dbb80acd46c16251c341bb40d7b
Alexander Mikhailov <support@indico.ru>
2014-10-08 16:32:13 +0400

    ex 3.26 - 3.29

--- src/funprog/Tree.scala
+++ src/funprog/Tree.scala
@@ -5,9 +5,15 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  // 3.25
-  def size[A](t: Tree[A]): Int = t match {
-    case Branch(l, r) => size(l) + size(r) + 1
-    case _ => 1
+
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+    case Leaf(v) => f(v)
   }
+
+   def size[A](t: Tree[A]): Int = fold(t)(_ => 1)((l, r) => l + r + 1)
+   def maximum(t: Tree[Int]): Int = fold(t)(x => x)((l, r) => l max r)
+   def depth[A](t: Tree[A]): Int = fold(t)(_ => 1)((l, r) => (l max r) + 1)
+   def map[A,B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(x => Leaf(f(x)): Tree[B])((l, r) => Branch(l, r))
+
 }
--- test/Chapter03.scala
+++ test/Chapter03.scala
@@ -1,141 +1,149 @@
 import org.scalatest._
-import funprog.List
-import funprog.List._
-
-import funprog.{Tree, Branch, Leaf}
-import funprog.Tree._
+import funprog.{List, Tree, Branch, Leaf}
 
 class Chapter03 extends FlatSpec with Matchers {
   val list = List(1, 2, 3)
 
-  "tail" should "get tail" in {
-    tail(list) should be (List(2,3))
+  "List.tail" should "get tail" in {
+    List.tail(list) should be (List(2,3))
   }
 
-  "drop" should "drop" in {
-    drop(list, 2) should be (List(3))
+  "List.drop" should "drop" in {
+    List.drop(list, 2) should be (List(3))
   }
 
   it should "not drop(0)" in {
-    drop(list, 0) should be (list)
+    List.drop(list, 0) should be (list)
   }
 
-  "setHead" should "sethead" in {
-     setHead(list, 4) should be (List(4,2,3))
+  "List.setHead" should "sethead" in {
+     List.setHead(list, 4) should be (List(4,2,3))
   }
 
-  "dropWhile" should "drop while predicate" in {
-    dropWhile(list)(_ < 3) should be (List(3))
+  "List.dropWhile" should "drop while predicate" in {
+    List.dropWhile(list)(_ < 3) should be (List(3))
   }
 
-  "append" should "append" in {
-    append(list, List(4,5)) should be (List(1,2,3,4,5))
+  "List.append" should "append" in {
+    List.append(list, List(4,5)) should be (List(1,2,3,4,5))
   }
 
-  "init" should "all but not last" in {
-    init(list) should be (List(1,2))
+  "List.init" should "all but not last" in {
+    List.init(list) should be (List(1,2))
   }
 
   // 3.11
-  "foldRight" should "able to sum" in {
-    foldRight(list, 0)(_ + _) should be (6)
+  "List.foldRight" should "able to sum" in {
+    List.foldRight(list, 0)(_ + _) should be (6)
   }
 
-  "foldRightViaReverse" should "able to sum" in {
-    foldRightViaReverse(list, 0)(_ + _) should be (6)
-  }
-  // 3.11
-  it should "able to product" in {
-    foldRight(list, 1)(_ * _) should be (6)
-  }
-  // 3.11
-  "foldLeft" should "able to sum" in {
-    foldLeft(list, 0)(_ + _) should be (6)
+  "List.foldRightViaReverse" should "able to sum" in {
+    List.foldRightViaReverse(list, 0)(_ + _) should be (6)
   }
   // 3.11
   it should "able to product" in {
-    foldLeft(list, 1)(_ * _) should be (6)
+    List.foldRight(list, 1)(_ * _) should be (6)
   }
   // 3.11
-  "len" should "compute length" in {
-    len(list) should be (3)
+  "List.foldLeft" should "able to sum" in {
+    List.foldLeft(list, 0)(_ + _) should be (6)
+  }
+  // 3.11
+  it should "able to product" in {
+    List.foldLeft(list, 1)(_ * _) should be (6)
+  }
+  // 3.11
+  "List.len" should "compute length" in {
+    List.len(list) should be (3)
   }
 
-  "reverse" should "reverse list" in {
-    reverse(list) should be (List(3,2,1))
+  "List.reverse" should "reverse list" in {
+    List.reverse(list) should be (List(3,2,1))
   }
 
-  "foldLeftViaFoldRight" should "able to sum" in {
-    foldLeftViaFoldRight(list, 0)(_ + _) should be (6)
+  "List.foldLeftViaFoldRight" should "able to sum" in {
+    List.foldLeftViaFoldRight(list, 0)(_ + _) should be (6)
   }
 
-  "foldRightViaFoldLeft" should "able to sum" in {
-    foldRightViaFoldLeft(list, 0)(_ + _) should be (6)
+  "List.foldRightViaFoldLeft" should "able to sum" in {
+    List.foldRightViaFoldLeft(list, 0)(_ + _) should be (6)
   }
 
-
-  "appendViaFoldLeft" should "append" in {
-    appendViaFoldLeft(list, List(4,5)) should be (List(1,2,3,4,5))
+  "List.appendViaFoldLeft" should "append" in {
+    List.appendViaFoldLeft(list, List(4,5)) should be (List(1,2,3,4,5))
   }
 
-  "appendViaFoldRight" should "append" in {
-    appendViaFoldRight(list, List(4,5)) should be (List(1,2,3,4,5))
+  "List.appendViaFoldRight" should "append" in {
+    List.appendViaFoldRight(list, List(4,5)) should be (List(1,2,3,4,5))
   }
 
-  "concat" should "concatenate lists" in {
-    concat(List(list, List(4,5))) should be (List(1,2,3,4,5))
+  "List.concat" should "concatenate lists" in {
+    List.concat(List(list, List(4,5))) should be (List(1,2,3,4,5))
   }
 
   // 3.16
-  "map" should "mapping values" in {
-    map(list)(_ + 1) should be (List(2,3,4))
+  "List.map" should "mapping values" in {
+    List.map(list)(_ + 1) should be (List(2,3,4))
   }
   // 3.17
   it should "mapping values to strings" in {
-    map(List(1.1, 2.2, 3.3))(_.toString) should be (List("1.1", "2.2", "3.3"))
+    List.map(List(1.1, 2.2, 3.3))(_.toString) should be (List("1.1", "2.2", "3.3"))
   }
 
-  "filter" should "filter values" in {
-    filter(list)(_ > 2) should be (List(3))
+  "List.filter" should "filter values" in {
+    List.filter(list)(_ > 2) should be (List(3))
   }
 
-  "flatMap" should "flattern mapping" in {
-    flatMap(List(1,2,3))(i => List(i,i)) should be (List(1,1,2,2,3,3))
+  "List.flatMap" should "flattern mapping" in {
+    List.flatMap(List(1,2,3))(i => List(i,i)) should be (List(1,1,2,2,3,3))
   }
 
-  "filterViaFlatMap" should "filter values" in {
-    filterViaFlatMap(list)(_ > 2) should be (List(3))
+  "List.filterViaFlatMap" should "filter values" in {
+    List.filterViaFlatMap(list)(_ > 2) should be (List(3))
   }
 
    // 3.22
-  "zipWith" should "zipping values with function" in {
-    zipWith(List(1,2,3), List(4,5,6))(_ + _) should be (List(5,7,9))
+  "List.zipWith" should "zipping values with function" in {
+    List.zipWith(List(1,2,3), List(4,5,6))(_ + _) should be (List(5,7,9))
   }
 
   it should "working on diferent sizes of lists" in {
-    zipWith(List(1,2), List(4,5,6))(_ + _) should be (List(5,7))
-    zipWith(List(1,2,3), List(4,5))(_ + _) should be (List(5,7))
+    List.zipWith(List(1,2), List(4,5,6))(_ + _) should be (List(5,7))
+    List.zipWith(List(1,2,3), List(4,5))(_ + _) should be (List(5,7))
   }
 
 
   // 3.24
-  "hasSubsequence" should "find subseqence" in {
-    hasSubsequence(List(1,2,3,4), List(1,2)) should be (true)
-    hasSubsequence(List(1,2,3,4), List(2,3)) should be (true)
-    hasSubsequence(List(1,2,3,4), List(3,4)) should be (true)
-    hasSubsequence(List(1,2,3,1,2,3,4,1), List(3,4)) should be (true)
+  "List.hasSubsequence" should "find subseqence" in {
+    List.hasSubsequence(List(1,2,3,4), List(1,2)) should be (true)
+    List.hasSubsequence(List(1,2,3,4), List(2,3)) should be (true)
+    List.hasSubsequence(List(1,2,3,4), List(3,4)) should be (true)
+    List.hasSubsequence(List(1,2,3,1,2,3,4,1), List(3,4)) should be (true)
   }
 
   it should "return false is not subsequence found" in {
-    hasSubsequence(List(1,2,3,4), List(2,1)) should be (false)
-    hasSubsequence(List(1,2,3,4), List(3,2)) should be (false)
-    hasSubsequence(List(1,2,3,4), List(1,3)) should be (false)
+    List.hasSubsequence(List(1,2,3,4), List(2,1)) should be (false)
+    List.hasSubsequence(List(1,2,3,4), List(3,2)) should be (false)
+    List.hasSubsequence(List(1,2,3,4), List(1,3)) should be (false)
   }
 
-
-  val t = Branch(Leaf(1), Leaf(2))
+  val t = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
 
   "Tree.size" should "return size of tree" in {
-    Tree.size(t) should be (3)
+    Tree.size(t) should be (5)
   }
+
+  "Tree.maximum" should "return max value in tree" in {
+    Tree.maximum(t) should be (3)
+  }
+
+  "Tree.depth" should "return max depth in tree" in {
+    Tree.depth(t) should be (3)
+  }
+
+  "Tree.map" should "map values in tree" in {
+    Tree.map(t)(_ + 1) should be (Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))
+  }
+
+
 }
